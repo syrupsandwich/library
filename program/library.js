acdb5a12c9b8da0201b50e7f76085082586a56eb
sau <dontsendsamspam@gmail.com>
2022-10-05 10:14:14 -0700

    Add books to myLibrary array

--- program/library.js
+++ program/library.js
@@ -8,9 +8,30 @@ function book(title, author, pages, status) {
 }
 
 book.prototype.info = function() {
-  return (`${this.title} by ${this.author}, ${this.pages}, ${this.status}`);
+  return (`${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`);
 }
 
 function addBookToLibrary(inputTitle, inputAuthor, inputPages, inputStatus) {
   myLibrary.push(new book(inputTitle, inputAuthor, inputPages, inputStatus));
+  myLibrary[myLibrary.length - 1].prototype = Object.create(book.prototype);
 }
+
+let successOneDayAtATime = new book('Success one day at a time', 'John C. Maxwell', 126, 'read');
+successOneDayAtATime.prototype = Object.create(book.prototype);
+myLibrary.push(successOneDayAtATime);
+
+let javascriptDefinitiveGuide = new book('JavaScript: The Definitive Guide', 'David Flanagan', 807, 'not yet read');
+javascriptDefinitiveGuide.prototype = Object.create(book.prototype);
+myLibrary.push(javascriptDefinitiveGuide);
+
+let mossflower = new book('Mossflower', 'Brian Jacques', 500, 'partially read');
+mossflower.prototype = Object.create(book.prototype);
+myLibrary.push(mossflower);
+
+let theWayOfMen = new book('The Way of Men', 'Jack Donovan', 192, 'read');
+theWayOfMen.prototype = Object.create(book.prototype);
+myLibrary.push(theWayOfMen);
+
+let aMindForNumbers = new book('A Mind for Numbers', 'Barbara Oakley, Ph.D.', 332, 'read');
+aMindForNumbers.prototype = Object.create(book.prototype);
+myLibrary.push(aMindForNumbers);
